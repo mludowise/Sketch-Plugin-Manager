1caf6690f774ad93740ccac7690bdb6ba9b8d8dd
Mel Ludowise <mludowise@gmail.com>
2017-06-21 13:36:18 -0700

    Bug fixes
    Bug fixes for #22, #25, & #29
    
    Updates to Chinese localization
    More robust logging & debug mode
    Stop checking deprecated plugin folders (resulted in window not displaying)
    Check for minimum OS X version

--- sketch-plugin-manager.sketchplugin/Contents/Sketch/main.js
+++ sketch-plugin-manager.sketchplugin/Contents/Sketch/main.js
@@ -1,29 +1,59 @@
 let kAutoReinstall = true
+let kDebug = false
 
 function run(context, appName, args) {
+    logInfo("run", true)
+
+    if(!hasMinOSXVersion()) {
+        return
+    }
+
     let sketch = context.api(),
         appUrl = sketch.resourceNamed(appName + ".app")
         pluginsFolder = getPluginsFolder(),
         pluginFile = context.plugin.url().path()
     args = args || []
 
-    args.concat([
+    args = args.concat([
         "-plugin-file", pluginFile,
         "-plugins-dir", pluginsFolder,
-        "-auto-reinstall", kAutoReinstall
+        "-auto-reinstall", kAutoReinstall.toString()
     ])
+
+    logInfo("Arguments: " + args, true)
 
     let options = { "NSWorkspaceLaunchConfigurationArguments": args }
     NSWorkspace.sharedWorkspace().launchApplicationAtURL_options_configuration_error(appUrl, NSWorkspaceLaunchDefault, options, null)
+    logInfo("end run", true)
+}
+
+function hasMinOSXVersion() {
+    let currentVersion = NSProcessInfo.processInfo().operatingSystemVersionString()
+    logInfo("System Version: " + currentVersion)
+
+    try {
+        if (NSAppKitVersionNumber >= NSAppKitVersionNumber10_12) {
+            return true
+        }
+    } catch (e) { }
+
+    alert("Sketch Plugin Manager requires OS X Version 10.12 (Sierra) or higher.\n\nYour system is running " + currentVersion, "Minimum System Requirements")
+
+    return false
 }
 
 function getPluginsFolder() {
     try {
+        let pluginsFolderURL = MSPluginManager.mainPluginsFolderURL()
+        logInfo("Plugins Folder URL: " + pluginsFolderURL, true)
+        logInfo("URL Class: " + pluginsFolderURL.class(), true)
+        logInfo("Plugins Folder: " + pluginsFolderURL.fileSystemRepresentation(), true)
+
         // Return default plugins folder from Sketch
-        return MSPluginManager.mainPluginsFolderURL().fileSystemRepresentation()
+        return pluginsFolderURL.fileSystemRepresentation()
     } catch(e) {
         // Falback in case Sketch Library changes
-        log(e)
+        logInfo(e)
         return "~/Library/Application Support/com.bohemiancoding.sketch3/Plugins/"
     }
 }
@@ -33,7 +63,7 @@ function debug(callback) {
         callback()
     } catch(e) {
 		if (e != null) { // Null error means plugin was exited without error
-			log(e)
+			logInfo(e)
 			alert(e, "Sketch Plugin Manager Error")
 		}
 	}
@@ -43,4 +73,12 @@ function alert(msg, title) {
 	title = title || "Error occurred in Sketch Plugin Manager"
 	var app = NSApplication.sharedApplication()
     app.displayDialog_withTitle(msg, title)
+}
+
+function logInfo(value, debug) {
+    if (debug && !kDebug) {
+        return
+    }
+
+    log(kAppBundleID + ": " + value)
 }
